3355d01e9e783fb2816ed98490c0724344e4e6c4
Nicholas <nicholaspesaresi@gmail.com>
2022-09-06 18:39:45 +0200

    task-2

--- main.py
+++ main.py
@@ -2,33 +2,51 @@ from ast import dump
 from audioop import reverse
 import re, json, requests
 
-from datetime import datetime
 
-# datetime object containing current date and time
-now = datetime.now()
-dt_string = now.strftime("%Y-%m-%d")
-print(dt_string)
+inputValue = input("For today data enter 'today' or press Enter to select another date:  ")
 
+# IF inputValue RECEIVES "today" AS VALUE THE JSON FILE RELATIVE TO 2022 IS FETCHED AND FILTERED, IF THE USER JUST PRESS ENTER, THEN HE CAN ENTER A DATE TO FILTER ANOTHER JSON FILE WITH DATA OF 2020 + 
 
-
-url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province-latest.json'
-
-resp = requests.get(url)
-resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
-data = json.loads(resp_parsed)
+if inputValue == "today":
+    url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province-latest.json'
+    resp = requests.get(url)
+    resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
+    data = json.loads(resp_parsed)
 
 
-regions_total_cases = [{"nome" : " Abruzzo", "totale" : 0}, {"nome" : "Basilicata", "totale" : 0}, {"nome" : "Calabria", "totale" : 0}, {"nome" : "Campania", "totale" : 0}, {"nome" : "Emilia-Romagna", "totale" : 0}, {"nome" : "Friuli_Venezia_Giulia", "totale" : 0}, {"nome" : "Lazio", "totale" : 0}, {"nome" : "Liguria", "totale" : 0}, {"nome" : "Lombardia", "totale" : 0}, {"nome" : "Marche" , "totale" :  0}, {"nome" : "Molise", "totale" : 0}, {"nome" : "P.A. Bolzano", "totale" : 0}, {"nome" : "P.A. Trento", "totale" :  0}, {"nome" : "Piemonte", "totale" : 0}, {"nome" : "Puglia", "totale" :  0}, {"nome" : "Sardegna", "totale" :  0}, {"nome" : "Sicilia", "totale" : 0}, {"nome" : "Toscana", "totale" : 0}, {"nome" : "Umbria", "totale" : 0}, {"nome" : "Valle d'Aosta", "totale" : 0} ]
+    regions_total_cases = [{"nome" : " Abruzzo", "totale" : 0}, {"nome" : "Basilicata", "totale" : 0}, {"nome" : "Calabria", "totale" : 0}, {"nome" : "Campania", "totale" : 0}, {"nome" : "Emilia-Romagna", "totale" : 0}, {"nome" : "Friuli_Venezia_Giulia", "totale" : 0}, {"nome" : "Lazio", "totale" : 0}, {"nome" : "Liguria", "totale" : 0}, {"nome" : "Lombardia", "totale" : 0}, {"nome" : "Marche" , "totale" :  0}, {"nome" : "Molise", "totale" : 0}, {"nome" : "P.A. Bolzano", "totale" : 0}, {"nome" : "P.A. Trento", "totale" :  0}, {"nome" : "Piemonte", "totale" : 0}, {"nome" : "Puglia", "totale" :  0}, {"nome" : "Sardegna", "totale" :  0}, {"nome" : "Sicilia", "totale" : 0}, {"nome" : "Toscana", "totale" : 0}, {"nome" : "Umbria", "totale" : 0}, {"nome" : "Valle d'Aosta", "totale" : 0} ]
 
-for i in data:
-    if "2022" in i["data"]:
+    for i in data:
         for r in regions_total_cases:
-                if i["denominazione_regione"] == r["nome"]:
-                    r["totale"] += i["totale_casi"]
+            if i["denominazione_regione"] == r["nome"]:
+                r["totale"] += i["totale_casi"]
 
 
-regions_total_cases.sort(key=lambda s: s["nome"], reverse=True)
+    regions_total_cases.sort(key=lambda s: s["nome"], reverse=True)
 
-regions_total_cases.sort(key=lambda s: s["totale"], reverse=True)
+    regions_total_cases.sort(key=lambda s: s["totale"], reverse=True)
 
-print(regions_total_cases)
+    print(regions_total_cases)
+
+else:
+    date = input("insert data yy-mm-dd: ")
+
+    url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json'
+    resp = requests.get(url)
+    resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
+    data = json.loads(resp_parsed)
+
+    regions_total_cases = [{"nome" : " Abruzzo", "totale" : 0}, {"nome" : "Basilicata", "totale" : 0}, {"nome" : "Calabria", "totale" : 0}, {"nome" : "Campania", "totale" : 0}, {"nome" : "Emilia-Romagna", "totale" : 0}, {"nome" : "Friuli_Venezia_Giulia", "totale" : 0}, {"nome" : "Lazio", "totale" : 0}, {"nome" : "Liguria", "totale" : 0}, {"nome" : "Lombardia", "totale" : 0}, {"nome" : "Marche" , "totale" :  0}, {"nome" : "Molise", "totale" : 0}, {"nome" : "P.A. Bolzano", "totale" : 0}, {"nome" : "P.A. Trento", "totale" :  0}, {"nome" : "Piemonte", "totale" : 0}, {"nome" : "Puglia", "totale" :  0}, {"nome" : "Sardegna", "totale" :  0}, {"nome" : "Sicilia", "totale" : 0}, {"nome" : "Toscana", "totale" : 0}, {"nome" : "Umbria", "totale" : 0}, {"nome" : "Valle d'Aosta", "totale" : 0} ]
+
+    for i in data:
+        if date in i["data"]:
+            for r in regions_total_cases:
+                    if i["denominazione_regione"] == r["nome"]:
+                        r["totale"] += i["totale_casi"]
+
+
+    regions_total_cases.sort(key=lambda s: s["nome"])
+
+    regions_total_cases.sort(key=lambda s: s["totale"], reverse=True)
+
+    print(regions_total_cases)
