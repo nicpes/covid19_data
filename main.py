1c8db7dea07cc8645a77ca41ec424f5ab126f771
Nicholas <nicholaspesaresi@gmail.com>
2022-09-06 19:04:01 +0200

    task-3

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from ast import dump
 from audioop import reverse
 import re, json, requests
+import pandas as pd
+
+
 
 
 inputValue = input("For today data enter 'today' or press Enter to select another date:  ")
@@ -14,7 +17,7 @@ if inputValue == "today":
     data = json.loads(resp_parsed)
 
 
-    regions_total_cases = [{"nome" : " Abruzzo", "totale" : 0}, {"nome" : "Basilicata", "totale" : 0}, {"nome" : "Calabria", "totale" : 0}, {"nome" : "Campania", "totale" : 0}, {"nome" : "Emilia-Romagna", "totale" : 0}, {"nome" : "Friuli_Venezia_Giulia", "totale" : 0}, {"nome" : "Lazio", "totale" : 0}, {"nome" : "Liguria", "totale" : 0}, {"nome" : "Lombardia", "totale" : 0}, {"nome" : "Marche" , "totale" :  0}, {"nome" : "Molise", "totale" : 0}, {"nome" : "P.A. Bolzano", "totale" : 0}, {"nome" : "P.A. Trento", "totale" :  0}, {"nome" : "Piemonte", "totale" : 0}, {"nome" : "Puglia", "totale" :  0}, {"nome" : "Sardegna", "totale" :  0}, {"nome" : "Sicilia", "totale" : 0}, {"nome" : "Toscana", "totale" : 0}, {"nome" : "Umbria", "totale" : 0}, {"nome" : "Valle d'Aosta", "totale" : 0} ]
+    regions_total_cases = [{"nome" : "Abruzzo", "totale" : 0}, {"nome" : "Basilicata", "totale" : 0}, {"nome" : "Calabria", "totale" : 0}, {"nome" : "Campania", "totale" : 0}, {"nome" : "Emilia-Romagna", "totale" : 0}, {"nome" : "Friuli Venezia Giulia", "totale" : 0}, {"nome" : "Lazio", "totale" : 0}, {"nome" : "Liguria", "totale" : 0}, {"nome" : "Lombardia", "totale" : 0}, {"nome" : "Marche" , "totale" :  0}, {"nome" : "Molise", "totale" : 0}, {"nome" : "P.A. Bolzano", "totale" : 0}, {"nome" : "P.A. Trento", "totale" :  0}, {"nome" : "Piemonte", "totale" : 0}, {"nome" : "Puglia", "totale" :  0}, {"nome" : "Sardegna", "totale" :  0}, {"nome" : "Sicilia", "totale" : 0}, {"nome" : "Toscana", "totale" : 0}, {"nome" : "Umbria", "totale" : 0}, {"nome" : "Valle d'Aosta", "totale" : 0} ]
 
     for i in data:
         for r in regions_total_cases:
@@ -28,6 +31,15 @@ if inputValue == "today":
 
     print(regions_total_cases)
 
+    excel_data  = pd.DataFrame(regions_total_cases)
+
+    excel = pd.ExcelWriter("covid_19_today_data.xlsx", engine="xlsxwriter")
+
+    excel_data.to_excel(excel, sheet_name="Sheet1")
+
+    excel.save()
+
+
 else:
     date = input("insert data yy-mm-dd: ")
 
@@ -36,7 +48,7 @@ else:
     resp_parsed = re.sub(r'^jsonp\d+\(|\)\s+$', '', resp.text)
     data = json.loads(resp_parsed)
 
-    regions_total_cases = [{"nome" : " Abruzzo", "totale" : 0}, {"nome" : "Basilicata", "totale" : 0}, {"nome" : "Calabria", "totale" : 0}, {"nome" : "Campania", "totale" : 0}, {"nome" : "Emilia-Romagna", "totale" : 0}, {"nome" : "Friuli_Venezia_Giulia", "totale" : 0}, {"nome" : "Lazio", "totale" : 0}, {"nome" : "Liguria", "totale" : 0}, {"nome" : "Lombardia", "totale" : 0}, {"nome" : "Marche" , "totale" :  0}, {"nome" : "Molise", "totale" : 0}, {"nome" : "P.A. Bolzano", "totale" : 0}, {"nome" : "P.A. Trento", "totale" :  0}, {"nome" : "Piemonte", "totale" : 0}, {"nome" : "Puglia", "totale" :  0}, {"nome" : "Sardegna", "totale" :  0}, {"nome" : "Sicilia", "totale" : 0}, {"nome" : "Toscana", "totale" : 0}, {"nome" : "Umbria", "totale" : 0}, {"nome" : "Valle d'Aosta", "totale" : 0} ]
+    regions_total_cases = [{"nome" : "Abruzzo", "totale" : 0}, {"nome" : "Basilicata", "totale" : 0}, {"nome" : "Calabria", "totale" : 0}, {"nome" : "Campania", "totale" : 0}, {"nome" : "Emilia-Romagna", "totale" : 0}, {"nome" : "Friuli Venezia Giulia", "totale" : 0}, {"nome" : "Lazio", "totale" : 0}, {"nome" : "Liguria", "totale" : 0}, {"nome" : "Lombardia", "totale" : 0}, {"nome" : "Marche" , "totale" :  0}, {"nome" : "Molise", "totale" : 0}, {"nome" : "P.A. Bolzano", "totale" : 0}, {"nome" : "P.A. Trento", "totale" :  0}, {"nome" : "Piemonte", "totale" : 0}, {"nome" : "Puglia", "totale" :  0}, {"nome" : "Sardegna", "totale" :  0}, {"nome" : "Sicilia", "totale" : 0}, {"nome" : "Toscana", "totale" : 0}, {"nome" : "Umbria", "totale" : 0}, {"nome" : "Valle d'Aosta", "totale" : 0} ]
 
     for i in data:
         if date in i["data"]:
@@ -50,3 +62,11 @@ else:
     regions_total_cases.sort(key=lambda s: s["totale"], reverse=True)
 
     print(regions_total_cases)
+
+    excel_data  = pd.DataFrame(regions_total_cases)
+
+    excel = pd.ExcelWriter("covid_19_data.xlsx", engine="xlsxwriter")
+
+    excel_data.to_excel(excel, sheet_name=f"covid_19_data")
+
+    excel.save()
